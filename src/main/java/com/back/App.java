67bf208c07d3b4a5d2e0ac916a5049e823d6d16c
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-04-23 16:32:52 +0900

    16강 - 함수 분리

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -17,26 +17,32 @@ public class App {
 			System.out.print("명령) ");
 			String input = sc.nextLine();
 			if (input.equals("등록")) {
-				System.out.print("명언) ");
-				String input_line = sc.nextLine();
-				System.out.print("작가) ");
-				String input_auther = sc.nextLine();
-				System.out.println(count + "번 명언이 등록되었습니다.");
-				WiseSaying newOne = new WiseSaying(count, input_auther, input_line);
-				wiseSayings.add(newOne);
-				count++;
-				continue;
+				register(wiseSayings, count);
 			} else if (input.equals("목록")) {
-				System.out.println("번호 / 작가 / 명언");
-				System.out.println("----------------------");
-				for (int i = wiseSayings.size() - 1; i >= 0; i--) {
-					WiseSaying auther = wiseSayings.get(i);
-					System.out.println(auther.num + " / " + auther.user + " / " + auther.comment);
-				}
-				continue;
+				showList(wiseSayings);
 			} else if (input.equals("종료")) {
 				break;
 			}
+		}
+	}
+
+	void register(ArrayList<WiseSaying> wiseSayings, int count) {
+		System.out.print("명언) ");
+		String input_line = sc.nextLine();
+		System.out.print("작가) ");
+		String input_auther = sc.nextLine();
+		System.out.println(count + "번 명언이 등록되었습니다.");
+		WiseSaying newOne = new WiseSaying(count, input_auther, input_line);
+		wiseSayings.add(newOne);
+		count++;
+	}
+
+	void showList(ArrayList<WiseSaying> wiseSayings) {
+		System.out.println("번호 / 작가 / 명언");
+		System.out.println("----------------------");
+		for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+			WiseSaying auther = wiseSayings.get(i);
+			System.out.println(auther.num + " / " + auther.user + " / " + auther.comment);
 		}
 	}
 }
