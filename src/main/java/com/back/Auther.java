7bfd90067cfb942107861ad6b10f5f9bc18409a2
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-04-23 14:48:35 +0900

    1단계-8단계 과제

---
+++ src/main/java/com/back/Auther.java
@@ -0,0 +1,15 @@
+package com.back;
+
+public class Auther {
+	int num;
+	String user;
+	String comment;
+
+	public Auther(int num, String user, String comment) {
+		this.num = num;
+		this.user = user;
+		this.comment = comment;
+	}
+
+	//1단계 - 8단계 구현!
+}
